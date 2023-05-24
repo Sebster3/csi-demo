041033243c1853bed34951760b210bd78138db46
Sebastien De La Cruz <sbas.alec@gmail.com>
2023-05-24 09:38:03 -0700

    Initial branch commit

---
+++ index.search.js
@@ -0,0 +1,79 @@
+var relearn_search_index = [
+  {
+    "content": "\rUnder Construction\rThis page under construction. Subject to change.\nNext Steps Create a New Customer Record Create a New Job Record ",
+    "description": "Get yourself up and running with Quickbooks",
+    "tags": null,
+    "title": "Getting Started",
+    "uri": "/quickbooks/qb-getting-started/index.html"
+  },
+  {
+    "content": "QuickBooks enables us to efficiently manage our finances, such as income, expenses, and cash flow, in a well-organized and centralized manner. With features like invoicing, bill payment, payroll, inventory tracking, and reporting, QuickBooks is a comprehensive tool that can help reduce errors and provide financial insights.\nIn many cases, it is possible to make edits after an entry has been made, but care should be taken to avoid this.\nGetting StartedGet yourself up and running with Quickbooks\nCreating a New CustomerLearn how to create a new Customer in Quickbooks\nCreating a New JobLearn how to create a new Job in Quickbooks\n",
+    "description": "Learn about the different tools you'll most commonly use within Quickbooks",
+    "tags": null,
+    "title": "Quickbooks",
+    "uri": "/quickbooks/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "\rUnder Construction\rThis page under construction. Subject to change.\nIn QuickBooks, a customer record is used to store and track information about a our customers, such as contact details, payment terms, and transaction history. By maintaining accurate customer records, we can send accurate invoices, monitor outstanding balances, and improve our relationships with our customers.\nWhat You’ll Need Customer Name Customer Address Customer Phone number Entering the Info Ensure Quickbooks is Open\rQuickBooks should be open and ready before proceeding. Check out QuickBooks: Getting Started if you need help.\nStep one Step 2 Supporting info Step 3 Sub-step 1 Sub-step 2 Sub-step 3 Sub-step 4 Step 4 ",
+    "description": "Learn how to create a new Customer in Quickbooks",
+    "tags": null,
+    "title": "Creating a New Customer",
+    "uri": "/quickbooks/qb-create-new-customer/index.html"
+  },
+  {
+    "content": "\rUnder Construction\rThis page under construction. Subject to change.\nIn QuickBooks, a job record serves as a way to track income and expenses for a specific project or job. It allows us to assign different transactions, such as invoices, bills, and payments, to a specific job and then view the profitability of that job in reports. By using job records, we can easily see which jobs are the most profitable, which jobs are over budget, and which clients owe money, among other insights.\nWhat You’ll Need Customer Name Job Name Job Address Estimator Initials Entering the Info Is Quickbooks open?\rQuickBooks should be open and ready before proceeding. Check out QuickBooks: Getting Started if you need help.\nCreating a new customer record\rNew customer? Follow the steps in Creating a New Customer\nSelect Customers from the left-hand panel. If the panel is hidden, expand it by clicking on the \u003e symbol near the upper-left of the window. Using the search box, look up the name of the customer. Right-Click on the customer’s name and select Add Job from the menu. In the Address Info tab, enter the information in the following format (replacing the #### with the job’s number): Job Name: #### - Job Name Ship to: #### - Job Name A popup titled “Customer Ship To Address Not Found” will appear, click Set Up. In the “Add Shiping Address Information” window, enter the following: Address Name: #### - Job Name Address: Job Name \u0026 Job Address on separate lines. Job name \u0026 address the same? Enter just the address. City, State, ZIP Ensure Default Shipping Address is checked. Click OK to save the new address. Click OK to save the new job. Done! ",
+    "description": "Learn how to create a new Job in Quickbooks",
+    "tags": null,
+    "title": "Creating a New Job",
+    "uri": "/quickbooks/qb-create-new-job/index.html"
+  },
+  {
+    "content": "\rUnder Construction\rThis page under construction. Subject to change.\nThe billing folder is broken down into the following nested folder structure:\n# Nested Folder structure Billing Customer Name Job folder # Example Billing Skier Construction 1234 - Customs Office Fence Builders 1235 - Shoreline 1242 - The Lab Folders in Detail Billing This is the top-level folder where all Customer folders can be found.\nCustomer Name This is the Customer-specific folder where all job folders for a given Customer can be found.\nJob Name This is the Job-specific folder where all documents for a given job can be found.\n",
+    "description": "Detailed breakdown of the billing folder's contents",
+    "tags": null,
+    "title": "Folder Structure",
+    "uri": "/billing-folder/billing-folder-structure/index.html"
+  },
+  {
+    "content": "\rUnder Construction\rThis page under construction. Subject to change.\nThe Job folder is broken down into the following nested folder structure:\n# Nested Folder levels Job Folder Billing SOV Invoices Releases Job Documents Contracts - Change Order Insurance Prelim Job Log Dailies Photos Plans - Engineering Estimator - PM Misc Numbers in Real Job Folders\rIn a real job folder, there are numbers in front of each subfolder’s name for easier navigation. They’ve been hidden here for cleaner presentation.\nFolders in Detail Billing This folder contains all billing-related documents such as invoices or releases.\n+++\nSOV TODO Invoices TODO Releases TODO Job Documents This folder contains all non-billing related job documents such as contracts or daily tickets.\nContracts - Change Order This folder contains contract-related documents such as the following:\nProposal Contract Sub Contract Change Orders Documents in this folder can, and should, be saved at the various stages of completion when they are made available. This includes when they are signed or fully executed.\nInsurance This folder contains insurance-related documents such as the following:\nInsurance requirements OCIP/UCIP Manual Certificate of Insurance The certificate of insurance should be named according to the following format:\nCOI - Skier Construction - 12345678 (22-23)\r↑ ↑ ↑\rCustomer Name Id Years Active See insurance for more info about the certificate of insurance.\nTODO: Link to page discussion insurance Prelim TODO Job Log TODO Dailies TODO Photos TODO Plans - Engineering TODO Estimator - PM TODO Misc TODO ",
+    "description": "Detailed breakdown of the job folder's contents",
+    "tags": null,
+    "title": "Folder Structure",
+    "uri": "/job-folder/job-folder-structure/index.html"
+  },
+  {
+    "content": "Home Welcome to our knowledge base!\nThis site is designed to be a central hub for all the information and resources you need to succeed in your role. Whether you’re a new employee looking to get up to speed or a seasoned veteran seeking new insights, you’ll find a wealth of valuable information here. We’re committed to providing our team with the tools and resources they need to excel, and we believe that this knowledge base is an essential part of that commitment. So, take a look around, explore the different sections, and let us know if there’s anything we can do to make this site even more helpful for you!\nQuickbooksLearn about the different tools you'll most commonly use within Quickbooks\nThe Billing FolderLearn about the structure and content within the billing folder\nThe Job Folder[Description]\n",
+    "description": "",
+    "tags": null,
+    "title": "Home",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "content": "The billing folder is the place where all job-related documents should be store, within their respective job folders.\nGet started on one of the following sections to learn more:\nFolder StructureDetailed breakdown of the billing folder's contents\n",
+    "description": "Learn about the structure and content within the billing folder",
+    "tags": null,
+    "title": "The Billing Folder",
+    "uri": "/billing-folder/index.html"
+  },
+  {
+    "content": "QuickBooks is where billing is handled and it’s important that all information be entered with as much accuracy as possible. In many cases, it is possible to make edits after an entry has been made, but care should be taken to avoid this.\nGet started on one of the following sections to learn more:\nFolder StructureDetailed breakdown of the job folder's contents\n",
+    "description": "[Description]",
+    "tags": null,
+    "title": "The Job Folder",
+    "uri": "/job-folder/index.html"
+  }
+]
